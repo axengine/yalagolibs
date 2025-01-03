a78a6be07f3b6852f38a5fd9b7715b9542dc865c
Lee0x273 <brucelee@yala.org>
2025-01-03 14:52:59 +0800

    add package log

---
+++ log/log.go
@@ -0,0 +1,57 @@
+package log
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+type (
+	aboveDebug struct{}
+	aboveWarn  struct{}
+)
+
+var Logger *zap.Logger
+
+func (l aboveDebug) Enabled(lv zapcore.Level) bool {
+	return lv >= zapcore.DebugLevel
+}
+
+func (l aboveWarn) Enabled(lv zapcore.Level) bool {
+	return lv >= zapcore.WarnLevel
+}
+
+func init() {
+	encoder := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+	wDebug := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   "./log/debug.log",
+		MaxSize:    100,
+		MaxBackups: 50,
+		MaxAge:     365,
+		LocalTime:  true,
+		Compress:   true,
+	})
+
+	wError := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   "./log/error.log",
+		MaxSize:    100,
+		MaxBackups: 10,
+		MaxAge:     365,
+		LocalTime:  true,
+		Compress:   true,
+	})
+
+	coreDebug := zapcore.NewCore(
+		encoder,
+		wDebug,
+		aboveDebug{},
+	)
+
+	coreError := zapcore.NewCore(
+		encoder,
+		wError,
+		aboveWarn{},
+	)
+
+	Logger = zap.New(zapcore.NewTee(coreDebug, coreError), zap.AddCaller(), zap.AddStacktrace(zap.DPanicLevel))
+}
