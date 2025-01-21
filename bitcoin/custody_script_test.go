138917d81018612e60f55af7ce3ef5d40b4a1afa
Lee0x273 <brucelee@yala.org>
2025-01-21 17:12:49 +0800

    feat:custody script tx size calc

--- bitcoin/custody_script_test.go
+++ bitcoin/custody_script_test.go
@@ -327,3 +327,39 @@ func TestClaimFromScriptx(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+// https://mempool.space/zh/testnet/tx/c985e52360cbb27085c7a8c7367c6c64376dcde5ce06024c2b908e835894555d
+func TestTxSizeWithinLocktime(t *testing.T) {
+	pk1 := "0x041813408b94aad6745f5710e887f3c705f9cce13e3c971f5e3347cfd06bbf64fb1594c4c25392a36284f26ec89e4bc490d3815562043de013795012a782c12125"     // tb1qnv34n2wcmd94leqqs3ycrwh93pa44adhexkj9l
+	pk2 := "0x04daae4253f0302725c22c9f124e6c0d182c5b4275047db181a567ff955aaebc19789155da6cd830ebc64e7ce13240c921b773681794f60e843d91d21b531584fb"     // tb1qvx32avcf6f6ke8s9h23pg7fr4d9798vs3xxrqr
+	pk3 := "0x040e1c9e63a03a4274998089d6491cd5ed789a7e09efbe7994a040228646e0e45d64def988296662eb5480ecab78dda694eda605bb9a56503533f02df3f5441202"     // tb1qpgw555fq567k098wtk6346ejt4q0jxhtxeyepc
+	pkAdmin := "0x043299eade0becea53c3e3943ccfbd647a103cf7654d8d7b79a11a6efeb81d8a31c396c7fa787f359608fe5b18debf4e8f242735f6049e880dd83480b403309a2a" // tb1qm39c3gupc2rfrrr6g8y8ely0ad26992lypcz5l
+	locktime := time.Date(2024, 12, 21, 9, 0, 0, 0, time.Local).Unix()
+
+	script, err := NewCustodyScript([]string{pk1, pk2, pk3}, pkAdmin, 2, 3, uint32(locktime), &chaincfg.TestNet3Params)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(script.DisasmString())
+	t.Log(script.Address())
+
+	t.Log(script.TxSizeWithinLocktime(1, 2))
+}
+
+// https://mempool.space/zh/testnet/tx/c67b7ba14cd1f64f3e7336e19548c0ac416be6d1b20a85d19afcc53a51061aee
+func TestTxSizeOutsideLocktime(t *testing.T) {
+	pk1 := "0x041813408b94aad6745f5710e887f3c705f9cce13e3c971f5e3347cfd06bbf64fb1594c4c25392a36284f26ec89e4bc490d3815562043de013795012a782c12125"     // tb1qnv34n2wcmd94leqqs3ycrwh93pa44adhexkj9l
+	pk2 := "0x04daae4253f0302725c22c9f124e6c0d182c5b4275047db181a567ff955aaebc19789155da6cd830ebc64e7ce13240c921b773681794f60e843d91d21b531584fb"     // tb1qvx32avcf6f6ke8s9h23pg7fr4d9798vs3xxrqr
+	pk3 := "0x040e1c9e63a03a4274998089d6491cd5ed789a7e09efbe7994a040228646e0e45d64def988296662eb5480ecab78dda694eda605bb9a56503533f02df3f5441202"     // tb1qpgw555fq567k098wtk6346ejt4q0jxhtxeyepc
+	pkAdmin := "0x043299eade0becea53c3e3943ccfbd647a103cf7654d8d7b79a11a6efeb81d8a31c396c7fa787f359608fe5b18debf4e8f242735f6049e880dd83480b403309a2a" // tb1qm39c3gupc2rfrrr6g8y8ely0ad26992lypcz5l
+	locktime := time.Date(2024, 12, 21, 9, 0, 0, 0, time.Local).Unix()
+
+	script, err := NewCustodyScript([]string{pk1, pk2, pk3}, pkAdmin, 2, 3, uint32(locktime), &chaincfg.TestNet3Params)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(script.DisasmString())
+	t.Log(script.Address())
+
+	t.Log(script.TxSizeOutsideLocktime(1, 1))
+}
