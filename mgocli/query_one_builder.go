bca3d9bd9a2dff3ca52ae5d8271d32123745c9c8
Lee0x273 <brucelee@yala.org>
2025-04-07 16:56:37 +0800

    update

--- mgocli/query_one_builder.go
+++ mgocli/query_one_builder.go
@@ -19,17 +19,20 @@ func NewQueryOneBuilder(collection string) *QueryOneBuilder {
 	}
 }
 
-func (b *QueryOneBuilder) Filter(filter bson.D) *QueryOneBuilder {
-	b.filter = filter
-	return b
-}
-
-func (b *QueryOneBuilder) FilterE(key string, value interface{}) *QueryOneBuilder {
+// Filter adds a filter condition to the query
+func (b *QueryOneBuilder) Filter(key string, value interface{}) *QueryOneBuilder {
 	b.filter = append(b.filter, bson.E{Key: key, Value: value})
 	return b
 }
 
-func (b *QueryOneBuilder) FilterElements(elements ...bson.E) *QueryOneBuilder {
+// FilterE adds a bson.E filter condition to the query
+func (b *QueryOneBuilder) FilterE(element bson.E) *QueryOneBuilder {
+	b.filter = append(b.filter, element)
+	return b
+}
+
+// FilterElements adds multiple bson.E filter conditions to the query
+func (b *QueryOneBuilder) FilterElements(elements []bson.E) *QueryOneBuilder {
 	b.filter = append(b.filter, elements...)
 	return b
 }
@@ -55,13 +58,14 @@ func (b *QueryOneBuilder) SetSkip(skip int64) *QueryOneBuilder {
 	return b
 }
 
-func (b *QueryOneBuilder) Projection(projection interface{}) *QueryOneBuilder {
-	b.opts.SetProjection(projection)
+func (b *QueryOneBuilder) Projection(fields bson.D) *QueryOneBuilder {
+	b.opts.SetProjection(fields)
 	return b
 }
 
-func (b *QueryOneBuilder) ProjectionE(key string, value interface{}) *QueryOneBuilder {
-	return b.Projection(bson.D{{Key: key, Value: value}})
+func (b *QueryOneBuilder) ProjectionE(elements ...bson.E) *QueryOneBuilder {
+	b.opts.SetProjection(bson.D(elements))
+	return b
 }
 
 func (b *QueryOneBuilder) Build() (string, bson.D, *options.FindOneOptionsBuilder) {
